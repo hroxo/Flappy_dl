5a403aeb7a1a23d591280cb178a3e0923e683d4e
hroxo <hugo.roxo04@gmail.com>
2025-11-19 15:15:52 +0000

    bird on image moving

--- game/bird.py
+++ game/bird.py
@@ -6,14 +6,14 @@
 #    By: hroxo <hroxo@student.42porto.com>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/11/19 13:15:19 by hroxo             #+#    #+#              #
-#    Updated: 2025/11/19 13:21:47 by hroxo            ###   ########.fr        #
+#    Updated: 2025/11/19 15:14:21 by hroxo            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import pygame
 
 class Birdy:
-    def __init__(self, img="./game/contents/bird.png"):
-        self.img = img
-    def bird_physics(self)
+    def __init__(self, bird_img="./game/contents/bird.png"):
+        self.bird_img = bird_img
+        self.bird_img = pygame.image.load(self.bird_img).convert_alpha()
 
--- game/main.py
+++ game/main.py
@@ -6,11 +6,14 @@
 #    By: hroxo <hroxo@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/11/19 12:19:04 by hroxo             #+#    #+#              #
-#    Updated: 2025/11/19 13:32:11 by hroxo            ###   ########.fr        #
+#    Updated: 2025/11/19 15:15:08 by hroxo            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import pygame
+from pygame.constants import K_SPACE, K_q
+from .bird import Birdy
+
 class DrawScreen:
     def __init__(self, height=800, width=800, img="./game/contents/background.png"):
         self.height = height
@@ -23,24 +26,23 @@ class DrawScreen:
         self.img = pygame.image.load(self.img)
         self.img = pygame.transform.scale(self.img, (self.width ,self.height))
         clock = pygame.time.Clock()
-    
+        birdimg = Birdy()
         running = True
-
+        i = 0
         while running:
-            # poll for events
-            # pygame.QUIT event means the user clicked X to close your window
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-            # fill the screen with a color to wipe away anything from last frame
-            screen.fill("blue")
             screen.blit(self.img, (0, 0))
-
-            # RENDER YOUR GAME HERE
-
-            # flip() the display to put your work on screen
+            screen.blit(birdimg.bird_img, (self.width / 3, i))
+            pygame.event.pump()
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == K_q:
+                        running = False
+                    else:
+                        i -= 200
+                elif event.type == pygame.QUIT: 
+                    running = False
             pygame.display.flip()
-
+            i += 10
             clock.tick(60)  # limits FPS to 60
 
         pygame.quit()
